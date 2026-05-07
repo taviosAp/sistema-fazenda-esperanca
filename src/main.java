2158efbaa7b07e8a6e73e7714183955e042781a4
MatheusCZago <mathzago@hotmail.com>
2026-05-07 19:36:31 -0300

    menus e cadastros base funcionando

--- src/main.java
+++ src/main.java
@@ -3,33 +3,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    static ArrayList<String> funcionarios = new ArrayList<>();
+    static ArrayList<Funcionario> funcionarios = new ArrayList<>();
+    static ArrayList<Talhao> talhoes = new ArrayList<>();
+    static ArrayList<Trator> tratores = new ArrayList<>();
+
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         int opcao;
 
         do {
-            System.out.println("\n1 - Cadastrar Funcionário");
+            System.out.println("\n=== SISTEMA FAZENDA ===");
+            System.out.println("1 - Cadastrar Funcionário");
+            System.out.println("2 - Cadastrar Talhão");
+            System.out.println("3 - Cadastrar Trator");
+            System.out.println("4 - Listar Funcionários");
+            System.out.println("5 - Listar Talhões");
+            System.out.println("6 - Listar Tratores");
             System.out.println("0 - Sair");
 
             opcao = sc.nextInt();
             sc.nextLine();
 
-            if (opcao == 1) {
-                cadastrarFuncionario();
+            switch (opcao) {
+                case 1 -> cadastrarFuncionario();
+                case 2 -> cadastrarTalhao();
+                case 3 -> cadastrarTrator();
+                case 4 -> listarFuncionarios();
+                case 5 -> listarTalhoes();
+                case 6 -> listarTratores();
             }
 
         } while (opcao != 0);
-    }
-
-    static void cadastrarFuncionario() {
-        System.out.print("Nome: ");
-        String nome = sc.nextLine();
-
-        funcionarios.add(nome);
-
-        System.out.println("Funcionário cadastrado!");
-        // ola
     }
 }
