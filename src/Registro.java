1bece3c18ec97bdcb99ad0862d409dc116021dea
MatheusCZago <mathzago@hotmail.com>
2026-05-13 19:39:34 -0300

    criando classe Registro

---
+++ src/Registro.java
@@ -0,0 +1,8 @@
+public class Registro {
+    public String data;
+    public int matriculaFuncionario;
+    public int codigoTalhao;
+    public String placaTrator;
+    public double litros;
+    public String destino;
+}
